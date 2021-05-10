6ea68bcb07d8c6ebc3832fb758ce40a357ed4e07
YOGESHWARAN R <yogeshin247@gmail.com>
2021-05-10 15:52:30 +0530

    Added Test

---
+++ test_rendering.py
@@ -0,0 +1,20 @@
+from contextlib import contextmanager
+
+import pytest
+from flask import request
+
+from app import app
+from github_utils import get_stats
+
+@pytest.fixture
+def client():
+    """ Client for Testing App """
+    with app.test_client() as client:
+        yield client
+
+
+def test_svg_rendering(client):
+    response = client.get('/yogeshwaran01?theme=powershell')
+    assert request.args['theme'] == 'powershell'
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'image/svg+xml; charset=utf-8'
